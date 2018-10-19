7fba09b31ebdbb3569b3b80eb96dccb73a134d37
g47923 <g47923@L405P17.pdg.esi.heb.site>
2018-10-19 17:15:12 +0200

    Correction des toString de style de vie

--- src/pkg47923_bmr/model/LifeStyle.java
+++ src/pkg47923_bmr/model/LifeStyle.java
@@ -7,11 +7,36 @@ package pkg47923_bmr.model;
  */
 public enum LifeStyle {
 
-    EXTREMELY_ACTIVE(1.9),
-    VERY_ACTIVE(1.725),
-    ACTIVE(1.55),
-    NOT_MUCH_ACTIVE(1.375),
-    SEDENTARY(1.2);
+    EXTREMELY_ACTIVE(1.9) {
+        @Override
+        public String toString() {
+            return "Extrêmement actif·ve";
+        }
+    },
+    VERY_ACTIVE(1.725) {
+        @Override
+        public String toString() {
+            return "Très actif·ve";
+        }
+    },
+    ACTIVE(1.55) {
+        @Override
+        public String toString() {
+            return "Actif·ve";
+        }
+    },
+    NOT_MUCH_ACTIVE(1.375) {
+        @Override
+        public String toString() {
+            return "Pas très actif·ve";
+        }
+    },
+    SEDENTARY(1.2) {
+        @Override
+        public String toString() {
+            return "Sédentaire";
+        }
+    };
 
     private final double factor;
 
